4983040473ae311471853025b508df2240e389a1
Daniel Oberle <danieloberle@MBP-von-Daniel.fritz.box>
2022-04-06 16:27:16 +0200

    Erster Server

---
+++ cmake-build-debug/CMakeFiles/PrakBS21.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS21.dir/main.c.o"
+  "CMakeFiles/PrakBS21.dir/main.c.o.d"
+  "PrakBS21"
+  "PrakBS21.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
