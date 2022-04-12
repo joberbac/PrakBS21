863ce8cbe88559583890e083b06c27eaffcffcc8
Dustin <83215125+dstein00@users.noreply.github.com>
2022-04-12 14:48:27 +0200

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,13 +1,5 @@
 //
-// Created by dusti on 05.04.2022.
+// Created by dustin on 05.04.2022.
 //
 #include "sub.h"
 
-
-int put(char* key, char* value);
-
-
-int get(char* key, char* res);
-
-
-int del(char* key);
