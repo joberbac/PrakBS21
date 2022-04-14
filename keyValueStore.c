8ce668aa25df77aa483676000b4121736799be6a
Dustin <83215125+dstein00@users.noreply.github.com>
2022-04-14 15:25:36 +0200

    Update keyValueStore.c

--- keyValueStore.c
+++ keyValueStore.c
@@ -3,9 +3,7 @@
 //
 #include <stdio.h>
 #include <string.h>
-
 #include "sub.h"
-
 #define MAX_KEY_LENGTH 10
 #define MAX_VALUE_LENGTH 100
 #define MAX_KEY_VALUE_STORE_SIZE 10
@@ -19,17 +17,12 @@ struct key_value_store {
 
 struct key_value_store key_val[MAX_KEY_VALUE_STORE_SIZE];
 
-void serverResponse(char* command, char* key, char* value, int curSocket);
-void getFileName(char* key, char* into);
 
 int put(char* key, char* value)
 {
     int curSocket = 4;
-
     char fileName[1024]={'\0'};	//key + ".txt" ending
     getFileName(key,fileName);
-
-    //printf("adding to filename => %s\n",fileName);
     FILE * file;
 
     /* open the file for writing*/
@@ -42,23 +35,18 @@ int put(char* key, char* value)
     fclose (file);
     strcat(value,"\n");
 
-    //printf("Key added\n");
     serverResponse("PUT", key, value, curSocket);
     return 1;
 }
 int get(char* key){
-
     int curSocket = 4;
-
     char fileName[1024]={'\0'};	//key + .txt ending
     getFileName(key,fileName);
 
-
     FILE *file;
     char readed[1024];
+    file = fopen (fileName, "r");
 
-    file = fopen (fileName, "r+");
-    //can be writen r instead of r+
     if (file != NULL)
     {
         while(fscanf(file, "%s", readed)!=EOF) ///EOF=End Of File
@@ -71,7 +59,6 @@ int get(char* key){
     }
     else
     {
-
         serverResponse("GET", key, "key_nonexistent\n", curSocket);
         return 1;
     }
@@ -86,11 +73,10 @@ int del(char* key)
 
     if (0==remove(fileName))
         serverResponse("DEL", key, "key_deleted\n", curSocket);
-        ///printf("The file is Deleted \n");
-
     else
         serverResponse("DEL", key, "key_nonexistent\n", curSocket);
-    ///printf("the file is NOT Deleted\n");
+
 }
+
 
 
