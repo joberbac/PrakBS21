4c9ef50744e17fa7ce9fe99c17d6bbc41cfd032d
Dustin <83215125+dstein00@users.noreply.github.com>
2022-04-19 15:50:12 +0200

    Update sub.c

--- sub.c
+++ sub.c
@@ -4,8 +4,8 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/mman.h>
 
+#include "sub.h"
 #include "keyValStore.h"
 
 #define MAX_INPUT_LENGTH 1000
@@ -19,65 +19,68 @@ void error_exit(char *error_message) {
     exit(EXIT_FAILURE);
 }
 
-//Erzeugt das Socket.
-int create_socket(int af, int type, int protocol) {
-    int sock;
+/*****************************************************************************************************************/
+/*************************************** Socket Systemaufrufe ****************************************************/
+/*****************************************************************************************************************/
 
-    if ((sock = socket(af, type, protocol)) < 0) {
-        error_exit("Fehler beim Anlegen eines Sockets\n");
+int create_socket(int af, int type, int protocol) {
+    int sock_fd;
+
+    if ((sock_fd = socket(af, type, protocol)) < 0) {
+        error_exit("Error occurred at create_socket\n");
     }
 
-    return sock;
+    return sock_fd;
 }
+
 
 //Weist dem Socket die IP-Adresse und die Portnummer zu, unter der das Socket auf Anfragen wartet.
-void bind_socket(int *sock, unsigned long address, unsigned short port) {
+void bind_socket(int *socket_fd, unsigned long address, unsigned short port) {
+
     struct sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(address);        //Die Kommunikationsschnittstelle des Hosts. Normalerweise die IP-Adresse.
-    server.sin_port = htons(port);                  //Transformiert die Portnummer in Network-Byte-Order.
+    server.sin_addr.s_addr = INADDR_ANY; //alle IP-Adressen des Rechners
+    server.sin_port = htons(port); //Transformiert die Portnummer in Network-Byte-Order.
 
-    if (bind(*sock, (struct sockaddr*)&server, sizeof (server)) < 0) {
-        error_exit("Kann das Socket nicht \"binden\"\n");
+    if (bind(*socket_fd, (struct sockaddr*)&server, sizeof (server)) < 0) { //hier das eigentliche Binden
+        error_exit("Error occurred at bind_socket\n");
     }
 }
+
 
 //Warteschlange, die auf eingehende Verbindungswünsche eines Clients wartet.
-void listen_socket(int *sock) {
-    if (listen(*sock, 5) == -1) {
-        error_exit("Fehler bei listen\n");
+void listen_socket(int *socket_fd) {
+    if (listen(*socket_fd, 5) == -1) { // 2. Parameter Warteschlangenlänge
+        error_exit("Error occurred at listen_socket\n");
     }
 }
 
-//Bearbeitet die Verbindungswünsche von Clients.
+
+//Verbindung eines Clients wird entgegengenommen
 //Der Aufruf von accept() blockiert so lange, bis ein Client Verbindung aufnimmt.
-void accept_socket(int *sock, int *fileDescriptor) {
+void accept_socket(int *socket_fd, int *cfd) {
     struct sockaddr_in client;
     unsigned int client_size = sizeof (client);
 
-    *fileDescriptor = accept(*sock, (struct sockaddr*)&client, &client_size);  //Rückgabewert von accept() ein neues Socket
+    *cfd = accept(*socket_fd, (struct sockaddr*)&client, &client_size);  //Rückgabewert von accept() ein neues Socket
 
-    if (*fileDescriptor == -1) {
-        error_exit("Fehler bei accept");
+    if (*cfd == -1) {
+        error_exit("Error occurred at accept_socket\n");
     }
+
 }
+
 
 //Socket schließen
-void close_socket(int *sock) {
-    close(*sock);
+void close_process(int *port) {
+    close(*port);
 }
 
+/*****************************************************************************************************************/
+/*************************************** Input Verarbeitung ******************************************************/
+/*****************************************************************************************************************/
 
-
-
-struct input{
-    char command_s[MAX_COMMAND_LENGTH];
-    char key_s[MAX_KEY_LENGTH];
-    char value_s[MAX_VALUE_LENGTH];
-};
-
-
-struct input * input_func(int *fileDescriptor) {
+struct input * input_func(int *cfd) {
 
     char command[MAX_COMMAND_LENGTH] = {};
     char key[MAX_KEY_LENGTH] = {};
@@ -86,38 +89,38 @@ struct input * input_func(int *fileDescriptor) {
 
     static struct input in;
 
-    int seperator[] = {0, 0};
+    int separator[] = {0, 0};
     int x = 0;
     int y = 0;
     int z = 0;
 
 
-    read(*fileDescriptor, buff, sizeof(buff));
+    read(*cfd, buff, sizeof(buff));
     buff[strlen(buff) - 2] = ' ';
 
 
     //Trennen des Strings
     for (int i = 0; i < strlen(buff); i++) {
         if (buff[i] == ' ') {
-            seperator[x] = i;
+            separator[x] = i;
             x++;
         }
     }
 
 
-    for (int i = 0; i < seperator[0]; i++) {
+    for (int i = 0; i < separator[0]; i++) {
         command[i] = buff[i];
     }
 
 
-    for (int i = seperator[0] + 1; i < seperator[1]; i++) {
+    for (int i = separator[0] + 1; i < separator[1]; i++) {
         key[y] = buff[i];
         y++;
     }
 
 
-    if (seperator[1] != 0) {
-        for (int i = seperator[1] + 1; i < strlen(buff) - 2; i++) {
+    if (separator[1] != 0) {
+        for (int i = separator[1] + 1; i < strlen(buff) - 2; i++) {
             value[z] = buff[i];
             z++;
         }
@@ -132,29 +135,29 @@ struct input * input_func(int *fileDescriptor) {
 }
 
 
-int execCommand(struct input *in, int *fileDescriptor, struct key_value_store *key_val) {
+int execCommand(struct input *in, int *cfd, struct key_value_store *key_val) {
     if (strcmp(in->command_s, "GET") == 0) {
-        return get(in->key_s, *fileDescriptor, key_val);
+        return get(in->key_s, *cfd, key_val);
     }
 
     else if (strcmp(in->command_s, "PUT") == 0) {
-        return put(in->key_s, in->value_s, *fileDescriptor, key_val);
+        return put(in->key_s, in->value_s, *cfd, key_val);
     }
 
     else if (strcmp(in->command_s, "DEL") == 0) {
-        return del(in->key_s, *fileDescriptor, key_val);
+        return del(in->key_s, *cfd, key_val);
     }
 
     else if (strcmp(in->command_s, "QUIT") == 0) {
-        close_socket(fileDescriptor);
-        printf("Verbindung zu Client getrennt\n");
+        close_process(cfd);
+        printf("Disconnected Client\n");
         return 2;
     }
 
     else {
-        printf("Falsche Eingabe\n");
-        if (send(*fileDescriptor, "Falsche Eingabe\n", sizeof ("Falsche Eingabe\n"), 0) == -1) {
-            printf("Fehler bei send()\n");
+        printf("Invalid Input\n");
+        if (send(*cfd, "Invalid Input\n", sizeof ("Invalid Input\n"), 0) == -1) {
+            printf("Error occurred at send\n");
         }
         return 0;
     }
